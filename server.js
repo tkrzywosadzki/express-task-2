8659049266d97592751e5c7762197c3fa77e73b2
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-07-31 21:32:56 +0200

    upload build

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const db = require('./db');
+const path = require('path');
 
 app.use(cors());
 
@@ -14,10 +15,19 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+
+
+  // Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+
 app.use((req, res) => {
     res.status(404).json({ message: "Not found..." });
   });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+app.listen(process.env.PORT || 8000, () => {
+console.log('Server is running on port: 8000');
 });
